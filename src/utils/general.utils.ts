80d0abe606761dcf5591792db2b17c7a40b3eb56
Robin Louis van Komen <rlvk@typoconsult.dk>
2022-08-03 10:12:41 +0200

    Add extension generation functionality

---
+++ src/utils/general.utils.ts
@@ -0,0 +1,23 @@
+export const convertSnakeCaseToKebabCase = (input: string): string => {
+    return input.replaceAll('_', '-').toLowerCase();
+};
+
+export const convertSnakeCaseToPascalCase = (input: string): string => {
+    return input.split('_').map(part => upperCaseFirst(part)).join('');
+};
+
+export const convertSnakeCaseToPretty = (input: string): string => {
+    const prettyParts = [ 'TC' ];
+    const upperCasedParts = input.split('_').map(part => upperCaseFirst(part));
+
+    upperCasedParts.shift();
+
+    prettyParts.push(...upperCasedParts);
+
+    return prettyParts.join(' ');
+};
+
+export const upperCaseFirst = (input: string): string => {
+    return input.slice(0, 1).toUpperCase() + input.slice(1, input.length);
+};
+
