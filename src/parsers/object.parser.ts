85fb40fa7d23ce3834816869fd5dad88c83e972b
Robin Louis van Komen <rlvk@typoconsult.dk>
2022-08-04 12:57:37 +0200

    Add temp message for object parsing

--- src/parsers/object.parser.ts
+++ src/parsers/object.parser.ts
@@ -1,13 +1,14 @@
-import { ParserInterface } from "../types/parser.types";
-import { InputInterface } from "../types/general.types";
+import chalk from 'chalk';
+import { ParserInterface } from '../types/parser.types';
+import { InputInterface } from '../types/general.types';
 
 class ObjectParser implements ParserInterface {
-    constructor(private readonly input: InputInterface) {
-    }
+    constructor(private readonly input: InputInterface) {}
 
     async parse(): Promise<void> {
-        console.log(this.input)
+        console.log(chalk.green(
+            'This feature is still in active development, and therefore does not generate any files yet.'));
     }
 }
 
-export default ObjectParser
+export default ObjectParser;
