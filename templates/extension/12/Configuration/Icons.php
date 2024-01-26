8d08d7263b9be8842afd0e1a6290b9d883f7c88e
Claus Harup <ch@typoconsult.dk>
2024-01-26 14:53:56 +0100

    Shortening code

--- templates/extension/12/Configuration/Icons.php
+++ templates/extension/12/Configuration/Icons.php
@@ -5,6 +5,4 @@ declare(strict_types=1);
 use TYPOCONSULT\extensionNamePascal\Constants\GeneralConstants;
 use TYPOCONSULT\TcTools\Services\IconLoaderService;
 
-$iconLoaderService = new IconLoaderService(GeneralConstants::EXT_KEY);
-
-return $iconLoaderService->readIconsFromFolder()->getIconsForIconsFile();
+return (new IconLoaderService(GeneralConstants::EXT_KEY))->readIconsFromFolder()->getIconsForIconsFile();
